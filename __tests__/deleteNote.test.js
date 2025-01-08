14e32287fd62d869cf72e08ccb21a169c7b0f4cb
NicolasJoubert <nicolasjoubert@icloud.com>
2025-01-08 11:27:00 +0100

    update test

--- __tests__/deleteNote.test.js
+++ __tests__/deleteNote.test.js
@@ -1,10 +1,21 @@
 const request = require('supertest');
 const app = require('../app');
 
-it('DELETE /delete/:noteId', async () => {
-  const noteId = '675c55d1e141037183c92d5d';
+const token = 'D9NhXav1YFFo2ZxD6xJ5lqNMEGLyGaio';
+let noteId = '';
 
-  const res = await request(app).delete(`/delete/${noteId}`);
+it('POST /', async () => {
+  // create new note
+  const res = await request(app).post('/notes').send({ token });
+  noteId = res.body.note._id; // // This value will be used for delete test
+
+  expect(res.statusCode).toBe(200);
+  expect(res.body.result).toBe(true);
+});
+
+it('DELETE notes/delete/:noteId', async () => {
+  // delete  note
+  const res = await request(app).delete(`/notes/delete/${noteId}`);
 
   expect(res.statusCode).toBe(200);
   expect(res.body.result).toBe(true);
